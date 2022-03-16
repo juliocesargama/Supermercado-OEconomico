04a39a50a70278cf1f7c1cd7f532eabfe0556ba6
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-16 16:49:51 -0300

    [feat] created Purchase repository

---
+++ src/main/java/br/com/repository/PurchaseRepository.java
@@ -0,0 +1,45 @@
+package br.com.repository;
+
+import br.com.interfaces.Crud;
+import br.com.models.Purchase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PurchaseRepository implements Crud<Purchase> {
+
+  private static ArrayList<Purchase> purchaseList = new ArrayList<>();
+
+
+  @Override
+  public Integer create(Purchase purchase) {
+    Purchase purchase1 = purchaseList.stream().filter(purchases -> purchases.getClient().getId() == purchase.getClient().getId()).findFirst().orElse(null);
+    if (purchase1 == null) {
+      purchaseList.add(purchase);
+      System.out.println("Purchase created");
+      return purchase.getId();
+    }
+    System.out.println("Client already exists");
+    return -1;
+  }
+
+  @Override
+  public Purchase update(Purchase purchase) {
+    return null;
+  }
+
+  @Override
+  public Boolean delete(Integer id) {
+    return purchaseList.remove(findById(id));
+  }
+
+  @Override
+  public Purchase findById(Integer id) {
+    return purchaseList.stream().filter(purchase -> purchase.getId() == id).findFirst().orElse(null);
+  }
+
+  @Override
+  public List<Purchase> getAll() {
+    return purchaseList;
+  }
+}
