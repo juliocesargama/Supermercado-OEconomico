0bfe77d2fc44246a675701268ecca744cb512c24
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-15 22:46:51 -0300

    [feat] refactoring the class Client access method - Lombok

--- src/main/java/br/com/client/Client.java
+++ src/main/java/br/com/client/Client.java
@@ -2,58 +2,21 @@ package br.com.client;
 
 import java.time.LocalDateTime;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Client {
-    private Integer id;
-    private String name;
-    private String CPF;
-    private String phone;
-    private LocalDateTime createdAt;
+    @Getter @Setter Integer id;
+    @Getter @Setter String name;
+    @Getter @Setter String CPF;
+    @Getter @Setter String phone;
+    @Getter @Setter LocalDateTime createdAt;
     
     public Client(int id, String name, String CPF, String phone, LocalDateTime createdAt) {
         this.id = id;
         this.name = name;
         this.CPF = CPF;
         this.phone = phone;
-        this.createdAt = createdAt;
-    }
-    
-    public int getId() {
-        return id;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public String getCPF() {
-        return CPF;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-    
-    public void setId(int id) {
-        this.id = id;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
 }
