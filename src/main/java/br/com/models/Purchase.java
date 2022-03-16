14f801ad3fa3da56facb67523d4b1bdd798c1ed5
Hugo Freire <ahugo.freire@gmail.com>
2022-03-16 18:17:41 -0300

    feat: refactor class Purchase add toString

--- src/main/java/br/com/models/Purchase.java
+++ src/main/java/br/com/models/Purchase.java
@@ -1,11 +1,9 @@
 package br.com.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -13,13 +11,14 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Purchase {
 
     private Integer Id;
     private Client client;
     private List<Item> items;
     private BigDecimal totalPrice;
-    private Date purchaseDate;
+    private LocalDate purchaseDate;
 
     public void calculaTotalPrice() {
         totalPrice = BigDecimal.valueOf(items.stream().mapToDouble(item -> item.getPrice().doubleValue()).sum());
---
+++ src/main/java/br/com/services/SupermarketService.java
@@ -0,0 +1,2 @@
+package br.com.services;public class supermarketService {
+}
