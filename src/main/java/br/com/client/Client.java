263affd05132390bdc34942b60cb06441686aa10
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-16 10:33:32 -0300

    [feat] put @AllArgsConstructor e @NoArgsConstructor on class client

--- src/main/java/br/com/client/Client.java
+++ src/main/java/br/com/client/Client.java
@@ -2,21 +2,19 @@ package br.com.client;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
     private Integer id;
     private String name;
     private String lastname;
     private LocalDateTime createdAt;
-    
-    public Client(int id, String name, String lastname, LocalDateTime createdAt) {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
-        this.createdAt = createdAt;
-    }
 }
