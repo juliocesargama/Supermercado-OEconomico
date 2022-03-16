02b6206c5fdd6623374ba13c008cf061f54dafdf
julisouza <julianoalcione@gmail.com>
2022-03-16 10:19:40 -0300

    Ajuste getters e setters

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -4,20 +4,13 @@ import lombok.Setter;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Getter @Setter
 public class Item {
-    @Getter @Setter
+
     private Integer id;
-
-    @Getter @Setter
     private String name;
-
-    @Getter @Setter
     private BigDecimal price;
-
-    @Getter @Setter
     private Integer quantity;
-
-    @Getter @Setter
     private Date expireDate;
 
     public Item(Integer id, String name, BigDecimal price, Integer quantity, Date expireDate) {
