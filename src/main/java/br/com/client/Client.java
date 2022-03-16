40ca808f220b1876f6abf0c2656be924b202c1d8
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-15 21:16:47 -0300

    [feat] create entity Client

--- src/main/java/br/com/client/Client.java
+++ src/main/java/br/com/client/Client.java
@@ -1,4 +1,42 @@
 package br.com.client;
 
 public class Client {
+    private int id;
+    private String name;
+    private String lastName;
+    
+    public Client(int id, String name, String lastName) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    @Override
+    public String toString() {
+        return "Client [id=" + id + ", name=" + name + ", lastName=" + lastName + "]";
+    }
 }
